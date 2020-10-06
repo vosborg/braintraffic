b34b99bbed958affb45f465d33d79da02949dbd0
vosborg <56126813+vosborg@users.noreply.github.com>
2020-10-06 11:56:27 +0200

    Add files via upload

---
+++ osborgtema/footer.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package mortens-tema
+ */
+?>
+	<footer id="colophon" class="site-footer">
+	<p> © 2020 Brain Traffic <a>Confab Conference</a> <a>Button Conference</a>  <a> Content Strategy  </a> <a>Podcast</a> <a>Contact Us</a>  <a>Newsletter</a> <a>Twitter</a> <a>LinkedIn</a> </p>
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+<?php wp_footer(); ?>
+<!-- Font Awesome-script -->
+<script src="https://kit.fontawesome.com/269c89033b.js" crossorigin="anonymous"></script>
+</body>
+</html>
